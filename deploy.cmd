00fe0ec4ba48d23f5d3c51c7c2be798879ba162a
Garry Taylor <giz@gbdesign.net>
2015-04-26 13:24:29 +0100

    Using Bower for Firebase depend. Include in CMD build and add a bower.json file

--- deploy.cmd
+++ deploy.cmd
@@ -105,6 +105,16 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
+:: 4. Install bower packages
+if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
+  cd "$DEPLOYMENT_TARGET"
+  eval $NPM_CMD install bower
+  exitWithMessageOnError "installing bower failed"
+  ./node_modules/.bin/bower install
+  exitWithMessageOnError "bower failed"
+  cd - > /dev/null
+fi
+
 :: 4. Build SASS and Minify
 :: pushd %DEPLOYMENT_TARGET%
 :: call :ExecuteCmd !NPM_CMD! install --development
